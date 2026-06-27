0f9b3600f6f277ad6816e3502e6c5e5cb0d40912
Api <4lbator.wun@gmail.com>
2026-06-27 22:52:37 +0200

    fix: add missing compileOnly dependencies to extension stub build configuration

--- extensions/bettertwitchtv/stub/build.gradle.kts
+++ extensions/bettertwitchtv/stub/build.gradle.kts
@@ -10,3 +10,13 @@ android {
         minSdk = 21
     }
 }
+
+dependencies {
+    compileOnly("io.reactivex.rxjava2:rxjava:2.2.21")
+    compileOnly("javax.inject:javax.inject:1")
+    compileOnly("androidx.recyclerview:recyclerview:1.2.1")
+    compileOnly("androidx.constraintlayout:constraintlayout:2.0.4")
+    compileOnly("androidx.fragment:fragment:1.3.6")
+    compileOnly("androidx.appcompat:appcompat:1.2.0")
+}
+
