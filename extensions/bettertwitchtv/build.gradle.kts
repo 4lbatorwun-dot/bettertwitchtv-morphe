496a7ba619d5b1e89855cb164436d7c8ce7520d7
Api <4lbator.wun@gmail.com>
2026-06-27 22:54:41 +0200

    fix: add Glide and transitive compileOnly dependencies to bettertwitchtv extension

--- extensions/bettertwitchtv/build.gradle.kts
+++ extensions/bettertwitchtv/build.gradle.kts
@@ -10,6 +10,12 @@ dependencies {
     compileOnly(project(":extensions:bettertwitchtv:stub"))
     compileOnly("androidx.appcompat:appcompat:1.2.0")
     compileOnly("com.google.android.material:material:1.3.0")
+    compileOnly("io.reactivex.rxjava2:rxjava:2.2.21")
+    compileOnly("javax.inject:javax.inject:1")
+    compileOnly("androidx.recyclerview:recyclerview:1.2.1")
+    compileOnly("androidx.constraintlayout:constraintlayout:2.0.4")
+    compileOnly("androidx.fragment:fragment:1.3.6")
+    compileOnly("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.github.zjupure:webpdecoder:2.0.4.12.0") {
         exclude(group = "com.github.bumptech.glide", module = "glide")
     }
